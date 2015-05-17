10b1b2adb9f9faf338787dff702658149ca0c3b9
jborseth <jborseth@users.noreply.github.com>
2015-05-17 19:10:27 -0400

    Clean up transactions/05_request_money.js

--- methods/transactions/05_request_money.js
+++ methods/transactions/05_request_money.js
@@ -1,24 +1,17 @@
 /**
  * Request bitcoin from an email address
- * Possibly also an account ID??
  *
  * Docs:
- * https://developers.coinbase.com/api#request-money
+ *   https://developers.coinbase.com/api#request-money
  * Lib:
- * Client.prototype.sendMoney
- * https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
+ *   Client.prototype.sendMoney
+ *   https://github.com/coinbase/coinbase-node/blob/master/lib/model/Account.js
  */
 
 var async = require('async');
 var Account = require('coinbase').model.Account;
 var client = require('../../client.js');
 
-var args = {
-// See docs for options
-            'to' : 'user1@example.com',
-            'amount' : '1.234',
-            'notes' : 'Sample transaction for you'
-         };
 
 async.waterfall([
   function(callback) {
@@ -27,7 +20,6 @@ async.waterfall([
       if (err) {
         console.log(err);
       } else {
-        // console.log(accounts);
         callback(null, accounts[0]);
       }
     });
@@ -37,8 +29,15 @@ async.waterfall([
     // Alternatively, you can manually specify an account ID if needed
     // var myAccount = new Account(client, {'id': 'A1234'});
 
+    // See docs for options
+    var args = {
+      to: 'user1@example.com',
+      amount: '1.234',
+      notes: 'Sample transaction for you'
+    };
+
     //Get a list of transactions
-    myAccount.sendMoney( args, function(err, txn) {
+    myAccount.sendMoney(args, function(err, txn) {
       if (err) {
         console.log(err);
       } else {
@@ -47,4 +46,3 @@ async.waterfall([
     });
   }
 ]);
-
