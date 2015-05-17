1af704214c859484b59a75625d2a5a526113c23b
jborseth <jborseth@users.noreply.github.com>
2015-05-17 12:45:41 -0400

    Add refunds/01_show_a_refunds.js

---
+++ methods/refunds/01_show_a_refunds.js
@@ -0,0 +1,11 @@
+/**
+ * Show a refund
+ *
+ * Docs:
+ *   https://developers.coinbase.com/api#show-a-refund
+ * Lib:
+ *   n/a
+ */
+
+var client = require('../../client.js');
+var async  = require('async');
