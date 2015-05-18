accb093358291fc6266543579bda573282ea492c
jborseth <jborseth@users.noreply.github.com>
2015-05-17 22:59:00 -0400

    Start clean up of transfers/03_start_a_transfer.js

--- methods/transfers/03_start_a_transfer.js
+++ methods/transfers/03_start_a_transfer.js
@@ -15,46 +15,52 @@ var async  = require('async');
 
 
 async.waterfall([
-  function(callback){
-  // Fetch an account ID
-    client.getAccounts(function(err, accounts){
-      if(err){
-        console.log(err.message);
+  function(callback) {
+    // Fetch an account ID
+    client.getAccounts(function(err, accounts) {
+      if (err) {
+        console.log(err);
       } else {
-        callback( null, accounts[0].id);
+        callback(null, accounts[0].id);
       }
     });
-  }, function( accountId, callback){
+  }, function(accountId, callback) {
 
     // Optionally, you can manually specify an account ID here if needed
     // var accountId = '';
 
-    var myAccount = new Account( client, {"id" : accountId });
+    var myAccount = new Account(client, {id: accountId});
 
-    //Create a buy/sell order using `commit: false`
-    //myAccount.sell( { qty: 10, currency: "USD", commit : false}, function(err, transfer){
-    myAccount.buy( { qty: 10, currency: "USD", commit : false}, function(err, transfer){
-      if (err){
+    var args = {
+      qty: 10,
+      currency: "USD",
+      commit: false
+    };
+
+    // Create a buy order or sell order using `commit: false`
+    // myAccount.sell(args, function(err, transfer) {
+    myAccount.buy(args, function(err, transfer) {
+      if (err) {
         console.log(err);
       } else {
-        console.log("Tranfer ID: " + transfer.id + ", type: " + transfer.type + ", detailed status: " +transfer.detailed_status);
+        // console.log("Tranfer ID: " + transfer.id + ", type: " + transfer.type + ", detailed status: " +transfer.detailed_status);
         callback( null, transfer.id, myAccount);
       }
     });
-  }, function(transferId, myAccount){
+  }, function(transferId, myAccount) {
 
     // Optionally, you can manually specify a transfer ID here if needed
     // var transferId = '';
 
-    var myTransfer = new Transfer(client, {id : transferId }, myAccount);
+    var myTransfer = new Transfer(client, {id : transferId}, myAccount);
 
-    myTransfer.commit( function(err, transfer){
-      if (err){
+    myTransfer.commit(function(err, transfer) {
+      if (err) {
         console.log(err);
       } else {
-        console.log("Tranfer ID: " + transfer.id + ", type: " + transfer.type + ", detailed status: " +transfer.detailed_status);
-        // console.log(transfer);
+        // console.log("Tranfer ID: " + transfer.id + ", type: " + transfer.type + ", detailed status: " +transfer.detailed_status);
+        console.log(transfer);
       }
     });
-  },
+  }
 ]);
